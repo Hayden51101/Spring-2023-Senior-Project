0041ed60eeba2f4fb064d82a9be3a78f9b951bf0
Hayden - Work <haydendustin11@gmail.com>
2025-03-07 14:59:31 -0500

    Fixed tempo algorithm

--- ImprovedAlgorithms/find_tempo.py
+++ ImprovedAlgorithms/find_tempo.py
@@ -25,6 +25,25 @@ def tempo_mode_test(mode_tempo: float, dilation: float, sensitivity: float, time
 
 
 
+def average(num_list: list) -> float:
+    if not num_list:
+        return 0
+    return sum(num_list) / len(num_list)
+
+
+
+def find_mode_index(list: list):
+    if not list:
+        return -1
+    index = 0
+    for i in range(len(list)):
+        if(len(list[i]) > len(list[index])):
+            index = i
+
+    return index
+
+
+
 def get_tempo(note_timestamps: list, notes_in_melody: list, key_of_melody: list, distance_sensitivity = 0.05, pass_threshold = 0.95, DEBUG = False) -> tuple[int, int]:
     """
     
@@ -43,37 +62,28 @@ def get_tempo(note_timestamps: list, notes_in_melody: list, key_of_melody: list,
             distance_between_notes.append(note - previous_note)
             previous_note = note
 
-    print("\n\n" + str(distance_between_notes) + "\n\n")
-
 
 
     note_distances = []
-    distance_occurances = []
 
     for current_distance in distance_between_notes:
 
         closest_distance_index = -1
         closest_distance = 100
         for i in range(len(note_distances)):
-            if abs(current_distance - note_distances[i]) < closest_distance:
+            if abs(current_distance - average(note_distances[i])) < closest_distance:
                 closest_distance_index = i
-                closest_distance = abs(current_distance - note_distances[i])
+                closest_distance = abs(current_distance - average(note_distances[i]))
 
         if closest_distance > distance_sensitivity:
-            note_distances.append(current_distance)
-            distance_occurances.append(1)
+            note_distances.append([current_distance])
 
         else:
-            distance_occurances[closest_distance_index]+=1
+            note_distances[closest_distance_index].append(current_distance)
 
 
 
-    for i in range(len(note_distances)):
-        print(str(note_distances[i]) + " - " + str(distance_occurances[i]))
-
-
-
-    mode_distance = note_distances[distance_occurances.index(max(distance_occurances))]
+    mode_distance = average(note_distances[find_mode_index(note_distances)])
 
 
 
